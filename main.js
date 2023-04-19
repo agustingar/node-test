824d4f369eff4076aa16f172f347d8a5f3c3b794
Agustín García Llorca <agustin.tavoite@gmail.com>
2023-04-19 12:20:45 +0200

    code styles and test

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const fs = require("fs");
+const fs = require('fs')
 
-
-const schema = require('./mock_application.json');
-const schemaJSON = JSON.parse(JSON.stringify(schema));
+const schema = require('./mock_application.json')
+const schemaJSON = JSON.parse(JSON.stringify(schema))
 
 class DeleteDuplicates {
     deleteObjectsFields(objects, isObjects = true) {
         Object.keys(objects).forEach((key) => {
-            const object = objects[key];
+            const object = objects[key]
             const objs = isObjects == true ? object.fields : object.views
-            
+
             for (let index = 0; index < objs.length; index++) {
-                const obj1 = objs[index];
-                
+                const obj1 = objs[index]
+
                 for (let index2 = 0; index2 < objs.length; index2++) {
-                    const obj2 = objs[index2];
-                    if (index != index2){
-                        if (obj1.key == obj2.key){
+                    const obj2 = objs[index2]
+                    if (index != index2) {
+                        if (obj1.key == obj2.key) {
                             objs.splice(index2, 1)
                             index2--
                             index--
@@ -24,25 +23,24 @@ class DeleteDuplicates {
                     }
                 }
             }
-        });
+        })
         return objects
-        
     }
 }
 
+let del = new DeleteDuplicates()
 
-
-let del = new DeleteDuplicates;
-
-schemaJSON.versions[0].objects = del.deleteObjectsFields(schemaJSON.versions[0].objects, true)
-schemaJSON.versions[0].scenes = del.deleteObjectsFields(schemaJSON.versions[0].scenes, false)
-
-
-
-
+schemaJSON.versions[0].objects = del.deleteObjectsFields(
+    schemaJSON.versions[0].objects,
+    true,
+)
+schemaJSON.versions[0].scenes = del.deleteObjectsFields(
+    schemaJSON.versions[0].scenes,
+    false,
+)
 
 const jsonString = JSON.stringify(schemaJSON)
-fs.writeFile('./clean_application.json', jsonString, err => {
+fs.writeFile('./clean_application.json', jsonString, (err) => {
     if (err) {
         console.log('Error writing file', err)
     } else {
--- test/converter.js
+++ test/converter.js
@@ -5,12 +5,12 @@ const schemaJSON = JSON.parse(JSON.stringify(schema));
 
 
 
-describe("Test main", function() {
-    it("Test the deleteObjectsFields function", function() {
+describe("Test main", function () {
+    it("Test the deleteObjectsFields function", function () {
         let del = new DeleteDuplicates;
-        schemaJSON.versions[0].objects =  del.deleteObjectsFields(schemaJSON.versions[0].objects, true)
-        schemaJSON.versions[0].scenes =  del.deleteObjectsFields(schemaJSON.versions[0].scenes, false)
-        
-        
+        schemaJSON.versions[0].objects = del.deleteObjectsFields(schemaJSON.versions[0].objects, true)
+        schemaJSON.versions[0].scenes = del.deleteObjectsFields(schemaJSON.versions[0].scenes, false)
+
+
     });
 });
